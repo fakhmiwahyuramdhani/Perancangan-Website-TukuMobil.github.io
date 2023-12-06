a7e437ab3e5b4624a8e3762271e3c930cd9f23e6
Fakhmi Wahyu Ramdhani <94135019+fakhmiwahyuramdhani@users.noreply.github.com>
2023-12-07 01:32:09 +0700

    first commit

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -11,10 +11,10 @@
     </div>
 
     <header style="text-align: center;">
-        <img src="frontend/imgs/dila.jpg" width="170" height="170" style="border-radius: 50%;" />
-        <h1>Fadila Rizka Nur Aminah</h1>
-        Student of informatics engineering
-        Politeknik Harapan Bersama Tegal
+        <img src="{{ asset('frontend/imgs/fakhmi.jpg') }}" width="170" height="170" style="border-radius: 40%;" />
+        <h1>Fakhmi Wahyu Ramdhani</h1>
+        Student of informatics
+        Universitas Gunadarma
     </header>
 
     <hr />
@@ -24,14 +24,14 @@
         <table width="100%">
             <tr>
 
-                    <p style="text-indent: 45px;">Website TukuMobil merupakan website yang saya buat untuk memenuhi nilai uas capston project semester 4.
+                    <p style="text-indent: 45px;">Website TukuMobil merupakan website yang saya buat untuk memenuhi nilai Ujian Praktikum, project semester 5.
                     Dalam website ini, saya mengimplementasikan fungsi CRUD (Create, Read, Update, Delete) yang memungkinkan admin untuk membuat, membaca,
                     mengubah, dan menghapus data mobil bekas. Admin dapat dengan mudah memasukkan informasi mengenai mobil yang ingin mereka jual, serta
                     pembeli dapat mencari mobil bekas yang sesuai dengan kebutuhan mereka. </p>
                     <p style="text-indent: 45px;">Selain fungsionalitas CRUD, saya juga berusaha menciptakan tampilan yang menarik dan intuitif.
                     tampilan ini saya dapat dari template surfide media, dan untuk admin saya menggunakan bootsrap sb admin.Pada website ini juga terdapat
                     satu fitur sistem pendukung keputusan, yaitu menentukan pembelian mobil bekas terbaik menggunakan metode SAW, namun karena keterbatasan,
-                    saya hanya menampilkan didalam menu admin.  </p>
+                    saya hanya menampilkan didalam menu admin. </p>
 
             </tr>
         </table>
